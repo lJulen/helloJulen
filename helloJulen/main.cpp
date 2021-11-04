cb2b82d81a073b6604a222c2e634b113a0e636f4
Julen Garro <julengarro@opendeusto.es>
2021-11-04 10:40:49 +0100

    Initial Commit

---
+++ helloJulen/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  helloJulen
+//
+//  Created by Julen Garro Olkotz on 4/11/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
